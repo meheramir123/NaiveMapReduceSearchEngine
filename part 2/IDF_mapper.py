f74ae7cf97f8208bcc4b5e12f8a64a70bc55c0f6
Meher Amir <118044508+meheramir123@users.noreply.github.com>
2024-03-30 22:27:51 +0500

    Add files via upload

---
+++ part 2/IDF_mapper.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import sys
+
+# Initialize a set to store encountered words
+encountered_words = set()
+
+# input comes from standard input
+for line in sys.stdin:
+    # parse the CSV line
+    words = line.strip().split()
+    # Emit each unique word along with a document ID
+    for word in words:
+        if word not in encountered_words:
+            encountered_words.add(word)
+            print('%s\t%s' % (word, 1))  # Emit word with count 1 for each document
+
